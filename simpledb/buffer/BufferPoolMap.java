4639583c0c3fd05f52035b4779e09a1f0aa48c9c
Jesse <jesse.xu.20@gmail.com>
2015-04-16 16:47:53 -0400

    add basic methods

---
+++ simpledb/buffer/BufferPoolMap.java
@@ -0,0 +1,17 @@
+package simpledb.buffer;
+
+import simpledb.file.Block;
+
+public class BufferPoolMap {
+
+	public boolean containsKey(Block blk) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Buffer get(Block blk) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
